b48c6e1b34f7e78d384b53e90536f27957a321dd
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-03-04 12:18:20 +0100

    Mobile devices style adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { ChakraProvider, Box, VStack, theme, Flex, Heading, Spacer, Button, Input, Text } from '@chakra-ui/react';
+import { ChakraProvider, Box, VStack, Flex, Heading, Spacer, Button, Input, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import theme from './theme';
 
 export const App = () => {
   const [message, setMessage] = useState('');
@@ -9,12 +10,14 @@ export const App = () => {
   const [socket, setSocket] = useState<Socket>();
 
   useEffect(() => {
-    const socketTemp = io('http://wschatserv.bieda.it');
+    const socketTemp = io('https://wschatserv.bieda.it');
     setSocket(socketTemp);
 
     socketTemp.on('msgToClient', (messageTemp) => {
       addMessage(messageTemp);
     });
+
+    document.addEventListener('keydown', handleKeyDown);
   }, []);
 
   const addMessage = (text: string) => {
@@ -23,6 +26,12 @@ export const App = () => {
 
   const sendMessage = () => {
     socket?.emit('msgToServer', message);
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
   };
 
   return (
@@ -34,8 +43,25 @@ export const App = () => {
           <ColorModeSwitcher justifySelf='flex-end' />
         </Flex>
 
-        <VStack w={'40%'} h={'30em'} borderWidth={'3px'} borderRadius={'5'} mx={'auto'} mt={10} p={2} gap={1}>
-          <VStack w={'100%'} borderWidth={'1px'} borderRadius={'5'} flexGrow={1} p={2}>
+        <VStack
+          w={['100%', '80%', '60%', '40%']}
+          h={'30em'}
+          borderWidth={'3px'}
+          borderRadius={'5'}
+          mx={'auto'}
+          mt={10}
+          p={2}
+          gap={1}
+        >
+          <VStack
+            w={'100%'}
+            borderWidth={'1px'}
+            borderRadius={'5'}
+            flexGrow={1}
+            p={2}
+            overflowY={'scroll'}
+            css={scrollbarStyle}
+          >
             {messages.map((message, id) => (
               <Box key={id} w={'100%'} borderWidth={'1px'} p={1} borderRadius={'5'} backgroundColor={'gray.900'}>
                 <Text>{message}</Text>
@@ -43,7 +69,13 @@ export const App = () => {
             ))}
           </VStack>
           <Flex w={'100%'}>
-            <Input placeholder='Message...' mr={2} value={message} onChange={(e) => setMessage(e.target.value)} />
+            <Input
+              placeholder='Message...'
+              mr={2}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyPress={handleKeyDown}
+            />
             <Button ml={2} flexShrink={0} onClick={sendMessage}>
               Send
             </Button>
@@ -52,4 +84,17 @@ export const App = () => {
       </Box>
     </ChakraProvider>
   );
+};
+
+const scrollbarStyle = {
+  '&::-webkit-scrollbar': {
+    width: '4px',
+  },
+  '&::-webkit-scrollbar-track': {
+    width: '6px',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    background: 'gray',
+    borderRadius: '24px',
+  },
 };
