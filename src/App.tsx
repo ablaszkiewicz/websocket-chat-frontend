49a3ffeffe1ae654f993f4db192fa64f7d504501
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-03-04 12:03:15 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,55 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ChakraProvider, Box, VStack, theme, Flex, Heading, Spacer, Button, Input, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+export const App = () => {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const [socket, setSocket] = useState<Socket>();
+
+  useEffect(() => {
+    const socketTemp = io('http://wschatserv.bieda.it');
+    setSocket(socketTemp);
+
+    socketTemp.on('msgToClient', (messageTemp) => {
+      addMessage(messageTemp);
+    });
+  }, []);
+
+  const addMessage = (text: string) => {
+    setMessages((messages) => [...messages, text]);
+  };
+
+  const sendMessage = () => {
+    socket?.emit('msgToServer', message);
+  };
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box p={5} minH='100vh'>
+        <Flex>
+          <Heading>Chat</Heading>
+          <Spacer />
+          <ColorModeSwitcher justifySelf='flex-end' />
+        </Flex>
+
+        <VStack w={'40%'} h={'30em'} borderWidth={'3px'} borderRadius={'5'} mx={'auto'} mt={10} p={2} gap={1}>
+          <VStack w={'100%'} borderWidth={'1px'} borderRadius={'5'} flexGrow={1} p={2}>
+            {messages.map((message, id) => (
+              <Box key={id} w={'100%'} borderWidth={'1px'} p={1} borderRadius={'5'} backgroundColor={'gray.900'}>
+                <Text>{message}</Text>
+              </Box>
+            ))}
+          </VStack>
+          <Flex w={'100%'}>
+            <Input placeholder='Message...' mr={2} value={message} onChange={(e) => setMessage(e.target.value)} />
+            <Button ml={2} flexShrink={0} onClick={sendMessage}>
+              Send
+            </Button>
+          </Flex>
         </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+      </Box>
+    </ChakraProvider>
+  );
+};
