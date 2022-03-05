ff0bafc05ef747d2bd1d1b81dce5cdbe0e96e164
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-03-05 17:51:05 +0100

    Style adjustments

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,8 @@
 import { InputPanel } from '../components/InputPanel';
 import { MessagesPanel } from '../components/MessagesPanel';
 import { Navbar } from '../components/Navbar';
-import { ChakraProvider, Box, VStack, Flex, Heading, Spacer, Button, Input, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { io, Socket } from 'socket.io-client';
+import { ChakraProvider, Box, VStack } from '@chakra-ui/react';
 import theme from '../theme';
-import { Message } from '../components/MessageListItem';
 
 export const App = () => {
   return (
@@ -15,7 +12,7 @@ export const App = () => {
 
         <VStack
           w={['100%', '80%', '60%', '40%']}
-          h={'30em'}
+          h={'60vh'}
           borderRadius={'5'}
           mx={'auto'}
           mt={10}
